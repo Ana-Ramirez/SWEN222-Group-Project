a752c11194406caaa6f44193a3e37eb8bb4cdc8f
Patrick Lynch <patrickjhlynch@gmail.com>
2017-09-19 01:11:36 +1200

    Started work on Renderer class

--- src/view/Renderer.java
+++ src/view/Renderer.java
@@ -1,7 +1,15 @@
 package view;
 
-public class Renderer {
+import java.awt.Graphics2D;
 
+public class Renderer {
+	private Graphics2D g;
+	private List<Entity> entities;
+	
+	
+	/**
+	 * Takes a list of entities to render and a Graphics2D object
+	 */
 	public Renderer(){
 		
 	}
