969cb3d170d5e9560ea31ccb40bc17e0d2ff24fb
LewisMcEwan <mcewanlewi@myvuw.ac.nz>
2017-09-19 21:19:53 +1200

    updated Controller class

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -2,8 +2,10 @@ package controller;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class Controller implements KeyListener{
+public class Controller implements KeyListener, MouseListener{
 	
 	public Controller(){
 		
@@ -34,5 +36,35 @@ public class Controller implements KeyListener{
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {}
+	
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
