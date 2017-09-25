9ae75fc3cd45d93550aef6e367f9fbe73b319524
Patrick Lynch <patrickjhlynch@gmail.com>
2017-09-25 21:36:36 +1300

    Slight change to main menu class - added start method

--- src/view/MainMenu.java
+++ src/view/MainMenu.java
@@ -5,6 +5,7 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 import saveLoad.Load;
 
 /**
@@ -12,9 +13,13 @@ import saveLoad.Load;
  * @author Patrick
  *
  */
-public class MainMenu {
+public class MainMenu extends Application{
 	Scene scene;
 	StackPane root;
+	
+	public void start(Stage primaryStage) throws Exception{
+		
+	}
 	
 	public MainMenu(){
 		root = new StackPane();
