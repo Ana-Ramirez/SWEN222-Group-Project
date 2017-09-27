8d1bc904f8f0feec331345e6300ec2acb71b9344
patrickjhlynch <patrickjhlynch@gmail.com>
2017-09-27 15:39:33 +1300

    Improvements to renderer and ImgResources

--- src/resources/ImgResources.java
+++ src/resources/ImgResources.java
@@ -1,7 +1,17 @@
 package resources;
 
 import java.awt.Image;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public enum ImgResources {
-	PIG("Pig.png");
+	MAINMENUBG("menuBackground.jpg");
+
+	public final Image img;
+
+	  ImgResources(String resourceName) {
+	    try{ img = ImageIO.read(ImgResources.class.getResource(resourceName)); }
+	    catch (IOException e){ throw new Error(e); }
+	  }
 }
