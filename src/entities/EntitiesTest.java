b50f84624c7f646eeb4522b97f49af3459c32629
Nick Lauder <nick.lauder@outlook.com>
2017-09-29 03:17:22 +1300

    Adding Tests

--- src/entities/EntitiesTest.java
+++ src/entities/EntitiesTest.java
@@ -8,19 +8,26 @@ public class EntitiesTest {
 	@Test
 	public void createPlayer() {
 		Player player = new Player(0,0);
-		assertEquals(player.getName(), "Tim");
-		assertEquals(player.getLives(), 3);
+		assertEquals("Tim", player.getName());
+		assertEquals(3, player.getLives());
 		assertNull(player.getType());
 	}
 	
 	@Test
 	public void createMonster() {
-		fail("Not yet implemented");
+		Weapon weapon = new MeleeWeapon("Sword", 0, 0, Type.WATER, 5);
+		Monster monster = new Monster("Baddie", 0, 0, Type.WATER, weapon);
+		assertEquals(100, monster.getLives());
+		assertEquals("Baddie", monster.getName());
+		assertNotSame(weapon, monster.getWeapon());
+		assertEquals(Type.WATER, monster.getType());
 	}
 	
 	@Test
 	public void createWeapon() {
-		fail("Not yet implemented");
+		Weapon weapon = new MeleeWeapon("Sword", 0, 0, Type.WATER, 5);
+		assertEquals("Sword", weapon.getName());
+		assertEquals(Type.WATER, weapon.getType());
 	}
 	
 	@Test
