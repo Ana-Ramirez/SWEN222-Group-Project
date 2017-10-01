ae0b1cd3b8da37b588496477cd5c3c8fce3d1ba4
LewisMcEwan <lewismcewan@Lewiss-MacBook-Pro.local>
2017-10-01 17:08:32 +1300

    Made tests for Level

--- src/tests/LevelTests.java
+++ src/tests/LevelTests.java
@@ -4,11 +4,43 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import entities.Player;
+import logic.Level;
+import logic.Room;
+
 public class LevelTests {
 
+	/**
+	 * Correct amount of rooms in Level
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void initialise1() {
+		Player player = new Player(100, 100);
+		Level level = new Level(player);
+		assertEquals(5, level.getRooms().size());
 	}
+	
+	/**
+	 * Get a room from the level
+	 */
+	@Test
+	public void initialise2() {
+		Player player = new Player(100, 100);
+		Level level = new Level(player);
+		Room room3 = level.getRoom(3);
+		assertEquals(room3, level.getRoom(3));
+	}
+	
+	/**
+	 * Test game not over on new level creation
+	 */
+	@Test
+	public void initialise3() {
+		Player player = new Player(100, 100);
+		Level level = new Level(player);
+		assertFalse(level.gameOver());
+	}
+	
+	
 
 }
