44118874d39dcfc16c3d0f0cfa429cb53e16ae71
TGastrell <tim.gastrell@gmail.com>
2017-10-01 17:22:10 +1300

    Made Game an application and modified button handling

--- src/game/LoadHandler.java
+++ src/game/LoadHandler.java
@@ -11,15 +11,24 @@ import saveLoad.Load;
 
 public class LoadHandler<T extends Event> implements EventHandler<ActionEvent>{
 
+	@SuppressWarnings("unused")
 	@Override
 	public void handle(ActionEvent event) {
 		FileChooser fc = new FileChooser();
+		if(fc == null) {
+			return;
+		}
 		File file = fc.showOpenDialog(new Stage());//I hope this works?
 		
 		Load load = new Load(file.getName());
 		Game game = (Game) load.loadGame();
 		
-		game.startGame();
+		try {
+			game.start(new Stage());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
