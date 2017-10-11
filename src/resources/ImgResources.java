56ba6c9a44685161036f061cc38c63b68a1194b0
patrickjhlynch <patrickjhlynch@gmail.com>
2017-10-11 15:56:07 +1300

    re-added images idk git removed them or something thanks obama

--- src/resources/ImgResources.java
+++ src/resources/ImgResources.java
@@ -32,6 +32,11 @@ public enum ImgResources implements Serializable{
 	WALLTOP("wallTop.png"),
 	//ENEMIES
 	MONSTER("monster.png"),
+	MAO("mao.png"),
+	//PROJECTILES
+	BULLET("bullet.png"),
+	HLASER("lazerHoriz.png"),
+	VLASER("lazerVert.png"),
 	//CONSUMABLES
 	POTION("potion.png"),
 	//PLAYER
