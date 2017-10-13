c36074e8d217a7563d2c2a1ab75c03e04f7feeb4
Nick Lauder <nick.lauder@outlook.com>
2017-10-13 14:00:02 +1300

    Getting rid of bad/pointless code

--- src/entities/Pickupable.java
+++ src/entities/Pickupable.java
@@ -16,13 +16,4 @@ public abstract class Pickupable extends MovableEntity {
 	protected boolean hit(int damage) {
 		return false;
 	}
-	
-	public void setX(double x) {
-		this.x = x;
-	}
-
-	public void setY(double y) {
-		this.y = y;
-		
-	}
 }
